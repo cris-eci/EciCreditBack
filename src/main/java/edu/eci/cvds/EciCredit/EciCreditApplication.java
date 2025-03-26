116f109d0b5c9a32e05fe93aa26bc962f60ce015
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-03-26 07:27:05 -0500

    Firts commit

---
+++ src/main/java/edu/eci/cvds/EciCredit/EciCreditApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.EciCredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/EciCredit/EciCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.EciCredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
